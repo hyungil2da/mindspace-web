db770b131a9a91a7b6780c7812302c4f94165412
노현길 <wesdzx741@naver.com>
2025-10-20 19:51:31 +0900

    ..

--- src/Menu/newsinfo.js
+++ src/Menu/newsinfo.js
@@ -3,14 +3,9 @@ export const newsUpdates = [
     yearMonth: "2025.07",
     items: [
       {
-        date: "2025.07.20",
-        version: "v0.5",
-        content: "EmotionVR 0.5 베타버전 출시!"
-      },
-      {
-        date: "2025.07.10",
-        version: "v0.4",
-        content: "웹 기능 작업 및 UI 디자인 개선"
+        date: "2025.05.01",
+        version: "v0.1",
+        content: "EmotionVR 프로젝트 개발 시작"
       },
       {
         date: "2025.05.20",
@@ -18,9 +13,44 @@ export const newsUpdates = [
         content: "감정 분석 기능 연동 및 테스트 완료"
       },
       {
-        date: "2025.05.01",
-        version: "v0.1",
-        content: "EmotionVR 프로젝트 개발 시작"
+        date: "2025.07.10",
+        version: "v0.4",
+        content: "웹 기능 작업 및 UI 디자인 개선"
+      },
+      {
+        date: "2025.07.20",
+        version: "v0.5",
+        content: "EmotionVR 0.5 베타버전 출시!"
+      },
+      {
+        date : "2025.07.25",
+        version : "v0.5.1",
+        content:"앱 버그 수정 및 게임 에셋 안정성 향상"
+      },
+      {
+        date: "2025.07.30",
+        version: "v0.6",
+        content: "피드백 반영 및 기능 개선 작업 시작"
+      },
+      {
+        date: "2025.09.15",
+        version: "v0.7",
+        content: "감정 계산 로직 재구성 및 VR게임 최적화"
+      },
+      {
+        date: "2025.10.05",
+        version: "v0.8",
+        content: "관리자페이지 및 홈페이지 리뉴얼 완료"
+      },
+      {
+        date: "2025.10.19",
+        version: "v0.9",
+        content: "EmotionVR 0.9 출시 - 최종 버그 수정 및 성능 향상"
+      },
+      {
+        date: "2025.10.20",
+        version: "v1.0",
+        content: "EmotionVR 1.0 출시 - 최종 버그 수정 및 성능 향상"
       }
     ]
   }
